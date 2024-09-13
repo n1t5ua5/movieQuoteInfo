243f25128d0ad0b1605a85de6cf1906768c61180
n1t5ua5 <nitsua5@proton.me>
2024-09-13 15:29:18 -0400

    first commit for movieQuoteInfo

---
+++ MovieQuoteInfo.java
@@ -0,0 +1,9 @@
+public class MovieQuoteInfo {
+
+    public static void main(String[] args) {
+        System.out.printIn("'You shall not pass!' - Gandalf, from the Lord of the Rings: the Fellowship of the Ring | 2001" );
+        System.out.printIn("'What are you doing here?' - Private Noguchi, from Kwaidan: The Snow Woman | 2015" );
+        System.out.printIn("'You stay classy San Diego!' - Ron Burgundy, from Anchorman: the Legend of Ron Burgundy | 2004" );
+        System.out.printIn("'The puppies are safe!' - Perdita from 101 Dalmations | 1996" );
+    }
+}
